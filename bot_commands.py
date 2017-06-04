f4b0d79acbe2059942bf25878ddda989572f2dab
relight31 <halfbcsina@yahoo.com.sg>
2017-06-04 11:40:26 +0800

    Main bot and commands

---
+++ bot_commands.py
@@ -0,0 +1,2 @@
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="i'm a bot")
---
+++ bot_main.py
@@ -0,0 +1,6 @@
+TOKEN = "387409480:AAHmD7cUiq6-Hotji7_bxikR_mkoM0W7GjM"
+dev_mode = True
+
+import telegram.ext
+updater = Updater(token = TOKEN)
+dispatcher = updater.dispatcher
