8e6b9c2a3544de610a067982fdc4dea47cffcfd2
relight31 <halfbcsina@yahoo.com.sg>
2017-06-16 11:50:34 +0800

    session creation, whoami diagnostic function

--- bot_commands.py
+++ bot_commands.py
@@ -1,2 +1,73 @@
+start_text = "Hi {}! I am the KE Band Room Bot.\
+\n\nI can assist you with booking the Band Room for rehearsals or trainings.\
+\n\nTo place a booking, type /newbooking."
+
+help_text = "Here is a list of commands you can give me.\
+\n\n/newbooking: Place a booking for the Band Room.\
+\n/checkstatus: Check if your bookings have been approved.\
+\n/amend: Amend details of your pending bookings. \
+(You may not amend bookings after they have been approved/rejected)\
+\n/cancel: Cancel any pending bookings you have placed.\
+\n\n/help: View this guide again."
+
+active_sessions = {}
+admin_id = 85548066
+unauth_user_msg = "Sorry {}, you do not have access to this function."
+def workinprogress(bot, update):
+    user_name = update.message.from_user.first_name
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry {}, this \
+    function is not ready yet.".format(user_name))
+
+def whoami(bot, update):
+    user_name = update.message.from_user.first_name
+    user_id = update.message.from_user.id
+    bot.send_message(chat_id=update.message.chat_id, \
+    text="user = {}, id = {}".format(user_name, user_id))
+
+def unknown(bot, update):
+    user_name = update.message.from_user.first_name
+    bot.send_message(chat_id=update.message.chat_id, \
+    text="Sorry {}, I do not understand that command.".format(user_name))
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="i'm a bot")
+    user_name = update.message.from_user.first_name
+    bot.send_message(chat_id=update.message.chat_id, \
+    text=start_text.format(user_name))
+
+def tutorial(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, \
+    text=help_text)
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+def open_session(bot, update):
+    id = update.message.from_user.id
+    user_name = update.message.from_user.first_name
+    if id in active_sessions:
+        bot.send_message(chat_id=update.message.chat_id, \
+        text="You already have an open session, {}.".format(user_name))
+        if dev_mode:
+            print("Session already open")
+    else:
+        session = {}
+        active_sessions[id] = session
+        bot.send_message(chat_id=update.message.chat_id, \
+        text="Session for {} created.".format(user_name))
+        if dev_mode:
+            print("Session created")
+
+def end_session(bot, update): #need to find new session tracking
+    id = update.message.from_user.id
+    user_name = update.message.from_user.first_name
+    if id in active_sessions:
+        del active_sessions[id]
+        bot.send_message(chat_id=update.message.chat_id, \
+        text="Your session was ended successfully.")
+        if dev_mode:
+            print("Session ended")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, \
+        text="You do not currently have an active session, {}.".format(user_name))
+        if dev_mode:
+            print("No active sessions")
--- bot_main.py
+++ bot_main.py
@@ -1,6 +1,44 @@
 TOKEN = "387409480:AAHmD7cUiq6-Hotji7_bxikR_mkoM0W7GjM"
 dev_mode = True
 
-import telegram.ext
+from bot_commands import *
+from telegram.ext import Updater
 updater = Updater(token = TOKEN)
 dispatcher = updater.dispatcher
+
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('help', tutorial)
+opensession_handler = CommandHandler('opensession', open_session)
+endsession_handler = CommandHandler("endsession", end_session)
+echo_handler = MessageHandler(Filters.text, echo)
+whoami_handler = CommandHandler("whoami", whoami)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+newbooking_handler = CommandHandler('newbooking', workinprogress)
+checkstatus_handler = CommandHandler('checkstatus', workinprogress)
+amend_handler = CommandHandler('amend', workinprogress)
+cancel_handler = CommandHandler('cancel', workinprogress)
+view_handler = CommandHandler('view', workinprogress)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(opensession_handler)
+dispatcher.add_handler(endsession_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(whoami_handler)
+
+dispatcher.add_handler(newbooking_handler)
+dispatcher.add_handler(checkstatus_handler)
+dispatcher.add_handler(amend_handler)
+dispatcher.add_handler(cancel_handler)
+dispatcher.add_handler(view_handler)
+
+dispatcher.add_handler(unknown_handler)
+
+### initialise bot ###
+if dev_mode:
+    print("Listening for new messages...")
+updater.start_polling()
